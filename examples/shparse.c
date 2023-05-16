6757bfed2e82510905ccd6d9ec68a6af4d3810d3
Eldyj <eldyj@tutanota.com>
2023-05-16 18:19:13 +0300

    shparse example update

--- examples/shparse.c
+++ examples/shparse.c
@@ -9,18 +9,27 @@ char
 	char *t0 = cstr_trim(s);
 	char *t1 = NULL;
 	size_t i = 0;
+	size_t o = 0;
 
 	char **a = malloc(sizeof(char*) * 1);
 	*a = NULL;
 	
 	while ((i = cstr_find_space(t0))) {
-		t1 = cstr_ltrim(t0+i);
-		cstrarr_append(&a, cstr_bashslice(t0, 0, i));
+		if (*t0 == '"') {
+			o = 1;
+			i = cstr_find_char(t0+o, '"');
+		}
+	
+		cstrarr_append(&a, cstr_bashslice(t0, o, i-1));
+		t1 = cstr_ltrim(t0+i+o);
 		free(t0);
 		t0 = t1;
+		o = 0;
 	}
 
-	cstrarr_append(&a, t0);
+	if (t0 != NULL && *t0 != '\0')
+		cstrarr_append(&a, t0);
+	
 	return a;
 }
 
@@ -28,13 +37,13 @@ int
 main(void)
 	/* VOID */
 {
-	char **t = shparse("./hello   69   96     10");
+	char **t = shparse("./hello  69 \"   6969  \" \"  123 \" 96 \" text \"");
 	const size_t l = cstrarr_len((const char *const *const) t);
 
 	size_t i = 0;
 	while (i < l) {
-		puts(t[i]);
-		free(t[i++]);
+		printf("'%s'\n", t[i++]);
+		//free(t[i++]);
 	}
 
 	free(t);
